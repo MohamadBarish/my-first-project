ecc21f85225f372a8ceaff28eb48888034dafbab
mohammed al azzo <mohammedalazzo@mohammeds-MacBook-Air.local>
2023-10-26 19:40:36 +0300

    i'm initing project

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+
+// pascalCassing
+function Message() {
+    //JSX: JavaScript XML 
+    const name = 'mohammad'; 
+    if (name)
+    return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
